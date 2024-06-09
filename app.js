e55b6b2b8f99f773ca99a5d8e8ac4dec78a3e8d5
masahiro1015 <masahiropper20021015@gmail.com>
2024-06-10 01:50:58 +0900

    change numbersize

--- app.js
+++ app.js
@@ -7,65 +7,104 @@ const multip = document.getElementById('multip');
 const divide = document.getElementById('divide');
 const plusminus = document.getElementById('plusminus');
 const percent = document.getElementById('percent');
+const point = document.getElementById('point');
 const c = document.getElementById('c');
 const ac = document.getElementById('ac');
 const equal = document.getElementById('equal');
 
-let way = '';
+let way = '0';
+const numarr = [];
+let pushnum = 0;
+
+const numsize = () => {
+    if (result.textContent.length >= 12) {
+        result.style.fontSize = '20px';
+        result.style.padding = '23px 30px';
+    }
+}
 
 // 数字のボタンを押したとき
 for (let num of nums) {
     num.addEventListener('click', () => {
+        numsize();
         if (result.textContent.length >= 26) {
             addEventListener(alert('あいうえお'));
         } else if (result.textContent === '0' && num.textContent === '0') {
         } else if (result.textContent === '0') {
             result.textContent = num.textContent;
-            way += num.textContent;
+            way = num.textContent;
+            pushnum = num.textContent;
         } else {
             result.textContent += num.textContent;
             way += num.textContent;
+            pushnum += num.textContent;
         }
     });
 }
 
 const funbtn = (s, mark) => {
     s.addEventListener('click', () => {
+        numsize();
         if (['+', 'ー', '×', '÷'].includes(result.textContent[result.textContent.length - 1])) {
             alert('無効な数式です');
         } else {
             way += mark;
             result.textContent += s.textContent;
+            numarr.push(pushnum);
+            numarr = '';
+            console.log(numarr);
         }
-    })
+    });
 }
 
 funbtn(plus, '+');
 funbtn(minus, '-');
 funbtn(multip, '*');
 funbtn(divide, '/');
-funbtn(percent, '*(1/100)');
+
+percent.addEventListener('click', () => {
+    numsize();
+    if (result.textContent === 0) return;
+    way += '*(1/100)';
+    result.textContent += percent.textContent;
+})
 
 plusminus.addEventListener('click', () => {
-    if(result.textContent === '0') return;
-    if(result.textContent[0] === '-') {
-        result.textContent = result.textContent.slice(1, result.textContent.length);
-    } else {
-        result.textContent = '-' + result.textContent;
-    }
+    if (result.textContent === '0') return;
+    // if (result.textContent[0] === '-') {
+    //     result.textContent = result.textContent.slice(1, result.textContent.length);
+    // } else {
+    //     result.textContent = '-' + result.textContent;
+    // }
     way += '*(-1)';
+    result.textContent += '×(-1)';
 });
 
+point.addEventListener('click', () => {
+    if(result.textContent.includes('.')) {
+        alert('無効な数式です');
+    } else {
+        way += '.';
+        result.textContent += '.';
+    }
+})
 
 c.addEventListener('click', () => {
-    way = way.slice(0, -1);
-    result.textContent = result.textContent.slice(0, -1);
+    if (result.textContent.length === 1) {
+        way = '0';
+        result.textContent = '0';
+    } else {
+        way = way.slice(0, -1);
+        result.textContent = result.textContent.slice(0, -1);
+    }
 });
 
 
 ac.addEventListener('click', () => {
     result.textContent = '0';
-    way = '';
+    result.style.fontSize = '40px';
+    result.style.padding = '10px 30px';
+    way = '0';
 });
 
 
